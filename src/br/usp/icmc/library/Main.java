46dce6ccf581875b3562dcaa356239fc8e8df6e7
gfceccon <gustavofceccon@live.com>
2015-05-18 18:12:54 -0300

    Package rename

--- src/br/usp/icmc/library/Main.java
+++ src/br/usp/icmc/library/Main.java
@@ -1,9 +1,6 @@
-package sample;
+package br.usp.icmc.library;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -12,9 +9,6 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 300, 275));
 		primaryStage.show();
 	}
 
