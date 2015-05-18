245675f4defcaf3ae498254bb1de7f9e2ebf8c6e
gfceccon <gustavofceccon@live.com>
2015-05-18 17:42:25 -0300

    Real start project

--- src/br/usp/icmc/library/Main.java
+++ src/br/usp/icmc/library/Main.java
@@ -2,13 +2,7 @@ package br.usp.icmc.library;
 
 public class Main
 {
-
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World é o caraio");
-		System.out.println("Hello World 2");
-	}
-
+    public static void main(String[] args)
+    {
+    }
 }
