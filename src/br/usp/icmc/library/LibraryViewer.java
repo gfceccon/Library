634fb6d0264d14575dd5cda577049870fbe85c17
gfceccon <gustavofceccon@live.com>
2015-05-29 21:50:58 -0300

    [ongoing] Add/Remove scenes

--- src/br/usp/icmc/library/LibraryViewer.java
+++ src/br/usp/icmc/library/LibraryViewer.java
@@ -15,10 +15,12 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 public class LibraryViewer extends Scene
 {
@@ -79,9 +81,12 @@ public class LibraryViewer extends Scene
 		Alert datePickerModal = new Alert(Alert.AlertType.CONFIRMATION);
 		DatePicker datePicker = new DatePicker();
 		datePickerModal.setTitle("Choose a date");
+		datePickerModal.setContentText("Choose a date:");
 		datePickerModal.getDialogPane().setContent(datePicker);
 		menuSetDate.setOnAction(event -> {
-			datePickerModal.showAndWait();
+			ButtonType returnValue = datePickerModal.showAndWait().get();
+			if(returnValue == null || returnValue.equals(ButtonType.CANCEL))
+				return;
 			try
 			{
 				LocalDate date = datePicker.getValue();
@@ -90,7 +95,6 @@ public class LibraryViewer extends Scene
 			}
 			catch (Exception e)
 			{
-				System.out.println(e.getMessage());
 				e.printStackTrace();
 			}
 		});
@@ -110,6 +114,23 @@ public class LibraryViewer extends Scene
 
 		addColumns();
 		fillTable();
+		setAddUserButton(addUser);
+
+		ButtonType returnValue = datePickerModal.showAndWait().get();
+		if(returnValue == null || returnValue.equals(ButtonType.CANCEL))
+			System.exit(0);
+		try
+		{
+
+			LocalDate date = datePicker.getValue();
+			controller.setDate(date);
+			currentDate.setText(date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 
 	private void setAddUserButton(Button button)
@@ -117,11 +138,76 @@ public class LibraryViewer extends Scene
 		Alert dialog = new Alert(Alert.AlertType.CONFIRMATION);
 		TextField usernameField = new TextField();
 		TextField nameField = new TextField();
-		ComboBox<String> typeComboBox = new ComboBox<>(FXCollections.observableArrayList("Student", "Teacher", "Comunity"));
+		ComboBox<String> typeComboBox = new ComboBox<>(FXCollections.observableArrayList("Student", "Teacher", "Community"));
+		TextField contactField = new TextField();
+		TextField emailField = new TextField();
+		TextField addressField = new TextField();
+		TextField cpfField = new TextField();
+
+		Label usernameLabel 	= new Label("Username");
+		Label nameLabel 		= new Label("Name");
+		Label typeLabel 		= new Label("Type");
+		Label contactLabel 		= new Label("Contact");
+		Label emailLabel 		= new Label("Email");
+		Label addressLabel 		= new Label("Address");
+		Label cpfLabel			= new Label("CPF");
+
 		GridPane pane = new GridPane();
 
+		pane.addColumn(0,
+				usernameLabel,
+				nameLabel,
+				typeLabel,
+				contactLabel,
+				emailLabel,
+				addressLabel,
+				cpfLabel);
+
+		pane.addColumn(1,
+				usernameField,
+				nameField,
+				typeComboBox,
+				contactField,
+				emailField,
+				addressField,
+				cpfField);
+
 		dialog.setTitle("Add user");
+		dialog.setContentText("User information");
 		dialog.getDialogPane().setContent(pane);
+
+		button.setOnAction(event -> {
+			Optional<ButtonType> returnValue = dialog.showAndWait();
+			if(returnValue.isPresent() && returnValue.get().equals(ButtonType.OK))
+			{
+				try
+				{
+					switch (typeComboBox.getValue())
+					{
+						case "Student":
+							controller.addStudent(
+									usernameField.getText(),
+									nameField.getText(),
+									contactField.getText(),
+									emailField.getText(),
+									addressField.getText(),
+									cpfField.getText());
+							break;
+						case "Teacher":
+							controller.addTeacher(usernameField.getText(), nameField.getText(), contactField.getText(), emailField.getText(), addressField.getText(), cpfField.getText());
+							break;
+						case "Community":
+							controller.addCommunity(usernameField.getText(), nameField.getText(), contactField.getText(), emailField.getText(), addressField.getText(), cpfField.getText());
+							break;
+					}
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+		});
 	}
 
 	private void setAddBookButton(Button button)
